e0fe62d97eb49a6cea7cd012cd479a17c6c52041
Md. Adib Khan <142529792+WebAdib@users.noreply.github.com>
2024-10-22 00:26:20 +0600

    deployment done

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
 import express from 'express'; // Import the Express framework
 import dotenv from "dotenv"; // Import dotenv to load environment variables from a .env file into process.env
+import path from 'path'; //
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+
 
 
 dotenv.config();// Load environment variables from the .env file (e.g., database connection URI)
 
 const app = express();// Initialize an Express application
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // allow us to access the json data
 
 app.use('/api/products', productRoutes);// Get products
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 
 
